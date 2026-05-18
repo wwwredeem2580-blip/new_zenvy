4174ed5c85ceac1c8728b9ea4b424351b593934d
wwwredeem2580-blip <www.redeem2580@gmail.com>
2026-05-18 15:19:11 +0600

    fix(client): bottom padding in layout to fix hide issue

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${dmSans.variable} ${lora.variable}`}>
-       <body className="antialiased font-sans">
+       <body className="antialiased font-sans pb-24">
          <ZenvyProvider>
            {children}
          </ZenvyProvider>
